fix duplicates + empty match + empty weight matrix + similarity normalization | add some few checks on input parameters

qbindiff/differ/preprocessing.py
@@ -53,38 +53,22 @@ def build_weight_matrix(features1: DataFrame, features2: DataFrame, distance: st
     Recall : the weights are to be MAXIMISED so they should computed according to a SIMILARITY measure (not a distance)
     """
     features1, features2 = process_features(features1, features2)
-    weight_matrix = cdist(features1, features2, distance)  # Compute distance
-    weight_matrix /=  weight_matrix.max()                  # Normalization
-    weight_matrix = 1 - weight_matrix                      # Distance to similarity
-    nsparse = int((1-threshold) * len(weight_matrix))
-    minrows = np.partition(weight_matrix, nsparse)[:,nsparse, None]
-    threshmask = weight_matrix >= minrows
+    weight_matrix = cdist(features1, features2, distance)            # Compute distance
+    weight_matrix /=  weight_matrix.max()                            # Normalization
+    weight_matrix = 1 - weight_matrix                                # Distance to similarity
+    #nsparse = int((1-threshold) * len(weight_matrix))
+    #minrows = np.partition(weight_matrix, nsparse)[:,nsparse, None]  # Set row threshold
+    #threshmask = weight_matrix >= minrows                            # Apply threshold
+    threshmask = weight_matrix >= threshold
     weight_matrix *= threshmask
     _compute_sparsity(threshmask)
-    colmask= threshmask.any(0)  # Keep vertex with at least one possible matching
+    colmask= threshmask.any(0)                                       # Keep vertex with at least one possible matching
     adds2 = features2.index[colmask]
     logging.debug("distance function pruning: p1: %d (after:%d), p2: %d (after: %d) [no match]" %
             (len(features1.index)-len(adds1), len(adds1), len(features2.index)-len(adds2), len(adds2)))
     weight_matrix = csr_matrix(weight_matrix[:, colmask])
     return adds1, adds2, weight_matrix
 
-
-def process_features(features1: DataFrame, features2: DataFrame) -> Tuple[DataFrame, DataFrame]:
-    opcmask = features1.astype(bool).sum(0) > 1   # remove features that only appears
-    opcmask &= features2.astype(bool).sum(0) > 1  # in one function or in one graph
-
-    (sh_ft1,_), (sh_ft2,_) = features1.shape, features2.shape
-    features1 = features1.loc[:, opcmask].drop_duplicates()  # remove duplicated rows
-    features2 = features2.loc[:, opcmask].drop_duplicates()  # (near-duplicate functions)
-    (sh2_ft1,_), (sh2_ft2,_) = features1.shape, features2.shape
-    logging.debug("function removed: p1:%d  (after:%d), p2: %d (after: %d) [duplicate features]" %
-                  (sh_ft1-sh2_ft1, sh2_ft1, sh_ft2-sh2_ft2, sh2_ft2))
-
-    opcsum = features1.sum(0) + features2.sum(0)
-    features1 /= opcsum  # feature ponderation via total
-    features2 /= opcsum  # number of appearance per features
-
-    return features1, features2
 
 def process_features(features1: DataFrame, features2: DataFrame) -> Tuple[DataFrame, DataFrame]:
     opcsum = features1.sum(0) + features2.sum(0)