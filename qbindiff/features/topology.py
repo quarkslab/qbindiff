867b5ca6f6a8ea44928c9cd37e5351d8cb74a58b
Robin David <rdavid@quarkslab.com>
2018-08-22 22:16:16 +0200

    remove not-working features for now

--- qbindiff/features/topology.py
+++ qbindiff/features/topology.py
@@ -1,5 +1,3 @@
-import networkx
-import community
 from qbindiff.features.visitor import FunctionFeatureExtractor, Environment
 from qbindiff.loader.function import Function
 
@@ -32,27 +30,3 @@ class NbFamily(FunctionFeatureExtractor):
     def call(self, env: Environment, fun: Function):
         n_family = len(fun.parents) + len(fun.children)
         env.add_feature('N_FAMILY', n_family)
-
-class NbGChildren(FunctionFeatureExtractor):
-    """Number of small children of the function"""
-    name = "nb_grand_children"
-    key = "Nbgc"
-
-    def call(self, env: Environment, fun: Function):
-        n_g_children = 0
-        for child in fun.children:
-            n_g_children += len(child.children)
-        env.add_feature('N_G_CHILDREN', n_g_children)
-
-
-class NbGParents(FunctionFeatureExtractor):
-    """Number of grand parents of the function"""
-    name = "nb_grand_parents"
-    key = "Nbgp"
-
-    def call(self, env: Environment, fun: Function):
-        n_g_parents = 0
-        for parent in fun.parents:
-            n_g_parents += len(parent.parents)
-        env.add_feature('N_G_PARENTS', n_g_parents)
-
