c23e82f4d28c7f75ab74f43153dfb638ea389469
elie <emengin@quarkslab.com>
2021-02-04 11:09:07 +0100

    fix mapping

--- qbindiff/mapping.py
+++ qbindiff/mapping.py
@@ -1,203 +1,216 @@
 from __future__ import absolute_import
 
-import json
-from collections import namedtuple
-
-from qbindiff.types import Addr, PathLike
-from typing import Optional, Set
+import sqlite3
 from pathlib import Path
 
-'''
-Match represent the matching between two functions and can hold the similarity between the two
-'''
-Match = namedtuple("Match", "addr_primary addr_secondary similarity")
+from typing import Tuple, List, Optional
+from qbindiff.types import PathLike, Mapping, Idx, Addr
+
 
 
 class Mapping:
-    def __init__(self, mapping):  # idx
-        self.raw_mapping = mapping
-        # TODO
-        pass
+    """
+    Matching hold all the match data between the two analysed programs
+    """
+    def __init__(self, mapping: Optional[Mapping]=None):
+        self._idx = []
+        self._idy = []
+        self._similarities = []
+        self._nb_squares = []
+        if mapping:
+            self.from_mapping(mapping)
 
-    def save(self, filename: PathLike) -> None:
-        p = Path(filename)
-        # TODO: Sérialiser l'objet en JSON puis l'écrire
+    def __iter__(self) -> Tuple[List[Idx], List[Idx]]:
+        return self.primary_match, self.secondary_matched
+
+    def from_mapping(self, mapping: Mapping):
+        idx, idy, _similarities, nb_squares = mapping
+        self._idx = list(idx)
+        self._idy = list(idy)
+        self._similarities = list(similarities)
+        self._nb_squares = list(nb_squares)
+
+    def from_file(self, filename: PathLike):
+        connect = sqlite3.connect(str(filename))
+        cursor = connect.cursor()
+        mapping = zip(*cursor.execute('SELECT address1, address2, similarity, squares FROM function').fetchall())
+        connect.close()
+        self.from_mapping(mapping)
+
+    def load(self, filename: PathLike)
+        self.from_file(filename)
+
+    def save(self, filename: PathLike):
+        if filename is None:
+            filename = '{}_vs_{}.qbindiff'.format(self.primary.name, self.secondary.name)
+        if os.path.exists(str(filename)):
+            os.remove(str(filename))
+        connect = sqlite3.connect(str(filename))
+        cursor = connect.cursor()
+        cursor.execute('DROP TABLE IF EXISTS function')
+        cursor.execute('CREATE TABLE function (address1 INTEGER, address2 INTEGER, similarity REAL, squares INTEGER)')
+        cursor.executemany('INSERT INTO function VALUES (?, ?, ?, ?)', zip(self.primary_matched, self.secondary_matched, self._similarities, self._nb_squares))
+        connect.commit()
+        connect.close()
+
+    def primary_match(self, idx: Idx) -> Idx:
+        """ Returns the match index associated with the given primary index """
+        if idx in self._idx:
+            return self._idy[self._idx.indice(idx)]
+        return None
+
+    def secondary_match(self, idx: Idx) -> Idx:
+        """ Returns the match index associated with the given secondary index """
+        if idx in self._idy:
+            return self._idx[self._idy.indice(idx)]
+        return None
+
+    @property
+    def primary_functions(self) -> List[Idx]:
+        """
+        Provide the list of all indexes in primary
+        :return: list of indexes in primary
+        WARNING: due to lack of information, this function might be erroneous
+        """
+        return list(range(max(self._idx)))
+
+    @property
+    def secondary_functions(self) -> List[Idx]:
+        """
+        Provide the list of all indexes secondary
+        :return: list of indexes in secondary
+        WARNING: due to lack of information, this function might be erroneous
+        """
+        return list(range(max(self._idy)))
+
+    @property
+    def primary_matched(self) -> List[Idx]:
+        """
+        Provide the list of indexes matched in primary
+        :return: list of indexes in primary
+        """
+        return self._idx
+
+    @property
+    def secondary_matched(self) -> List[Idx]:
+        """
+        Provide the list of indexes matched secondary
+        :return: list of indexes in secondary
+        """
+        return self._idy
+
+    @property
+    def primary_unmatched(self) -> List[Idx]:
+        """
+        Provide the list of indexes unmatched primary
+        :return: list of indexes in primary
+        """
+        return list(set(self.primary_functions).difference(self.primary_matched))
+
+    @property
+    def secondary_unmatched(self) -> List[Idx]:
+        """
+        Provide the list of indexes unmatched secondary
+        :return: list of indexes in secondary
+        """
+        return list(set(self.secondary_functions).difference(self.secondary_matched))
+
+    @property
+    def nb_primary_functions(self) -> int:
+        """ Total number of function in primary """
+        return len(self.primary_functions)
+
+    @property
+    def nb_secondary_functions(self) -> int:
+        """ Total number of function in secondary """
+        return len(self.secondary_functions)
+
+    @property
+    def nb_matched(self) -> int:
+        """ Returns the number of matches """
+        return len(self._idx)
+
+    @property
+    def nb_primary_unmatched(self) -> int:
+        """ Number of unmatched function in the primary """
+        return self.nb_primary_functions - self.nb_matched
+
+    @property
+    def nb_secondary_unmatched(self) -> int:
+        """ Number of unmatched function in the secondary """
+        return self.nb_secondary_functions - self.nb_matched
+
+    @property
+    def similarity(self) -> float:
+        """ Global similairty score of the mapping """
+        return sum(self._similarities)
+
+    @property    
+    def nb_squares(self) -> int:
+        """ Global square number of the mapping """
+        return sum(self._nb_squares) / 2
+
+    @property    
+    def score(self, tradeoff: float=.5) -> float:
+        """ Global network alignment score of the mapping """
+        return tradeoff * self.similarity + (1 - tradeoff) * self.nb_squares
+
+    def display_statistics(self, mapping: Optional[Mapping]=None):
+        output = 'Score: {:.4f} | Similarity: {:.4f} | '\
+                 'Squares: {:.0f} | Nb matches: {}\n'.format(self.score, self.similarity, self.nb_squares, self.nb_matched)
+        return output
 
 
 
-class AddressMapping(Mapping):
-    def __init__(self, p1, p2, mapping):
-        super(ProgramMapping, self).__init__(mapping)
-        self.primary = p1
-        self.secondary = p2
+class FunctionMapping(Mapping)
+
+    def __init__(self, primary: Program, secondary: Program,  mapping: Mapping=None):
+        super().__init__(mapping)
+        self._primary = primary
+        self._secondary = secondary
+        self._primary_index = dict(zip(*enumerate(self._primary)))
+        self._secondary_index = dict(zip(*enumerate(self._secondary)))
+
+    @property
+    def primary_functions(self) -> List[Addr]:
+        """ Total number of function in primary """
+        return list(self._primary_index.values())
+
+    @property
+    def secondary_functions(self) -> List[Addr]:
+        """ Total number of function in secondary """
+        return list(self._secondary_index.values())
+
+    @property
+    def primary_matched(self) -> List[Addr]:
+        """
+        Provide the set of indexes matched in primary
+        :return: set of indexes in primary
+        """
+        return [self._primary_index[idx] for idx in self._idx]
+
+    @property
+    def secondary_matched(self) -> List[Addr]:
+        """
+        Provide the set of indexes matched in the secondary binary
+        :return: set of indexes in secondary
+        """
+        return [self._secondary_index[idy] for idy in self._idy]
+
+    def display_statistics(self, mapping: Optional[Mapping]=None):
+        output = super().display_statistics(mapping)
+        output += 'Node cover:  {:.3f}% / {:.3f}% | '\
+                  'Edge cover:  {:.3f}% / {:.3f}%\n'.format(100 * self.nb_matched / self.nb_primary_functions,
+                                                            100 * self.nb_matched / self.nb_secondary_functions,
+                                                            100 * self.nb_squares / self._primary.callgraph.sum(),
+                                                            100 * self.nb_squares / self._secondary.callgraph.sum())
+        return output
 
 
 class BasicBlockMapping(AddressMapping):
     pass
 
+
 class FunctionMapping(AddressMapping):
     pass
 
-
-
-class ProgramMapping:
-    """
-    Matching hold all the match data between the two analysed programs
-    """
-    def __init__(self, primary_set: Set[Addr]=None, secondary_set: Set[Addr]=None, file: str=None):
-        self.primary_idx = {}
-        self.secondary_idx = {}
-        self.unmatched_primary = primary_set if primary_set else set()
-        self.unmatched_secondary = secondary_set if secondary_set else set()
-        self.global_sim = 0
-
-        if file:
-            self.load_file(file)
-
-    def __iter__(self):
-        for i in self.primary_idx.values():
-            yield i
-
-    @property
-    def similarity(self) -> float:
-        """ Global similarity of the diff """
-        return self.global_sim
-
-    @similarity.setter
-    def similarity(self, value: float) -> None:
-        """ Setter for the global similarity """
-        self.global_sim = value
-
-    @property
-    def matching(self):
-        """
-        Provide the matching as a dictionnary from primary addresses to secondary addresses
-        :return: dict
-        """
-        return {x.addr_primary: x.addr_secondary for x in self.primary_idx.values()}
-
-    def add_match(self, addr_p1: Addr, addr_p2: Addr, similarity: float=None) -> None:
-        """
-        Add the given match between the two function addresses
-        :param addr_p1: function address in primary
-        :param addr_p2: function address in secondary
-        :param similarity: similarity metric as float
-        :return: None
-        """
-        match = Match(addr_p1, addr_p2, float("{0:.2f}".format(similarity)))
-        self.primary_idx[addr_p1] = match
-        self.secondary_idx[addr_p2] = match
-        self.unmatched_primary.remove(addr_p1)
-        self.unmatched_secondary.remove(addr_p2)
-
-    def remove_match(self, match: Match) -> None:
-        """
-        Remove the given matching from the matching
-        :param match: Match object to remove from the matching
-        :return: None
-        """
-        self.primary_idx.pop(match.addr_primary)
-        self.unmatched_primary.add(match.addr_primary)
-        self.secondary_idx.pop(match.addr_secondary)
-        self.unmatched_secondary.add(match.addr_secondary)
-        del match
-
-    @property
-    def primary_address_matched(self) -> Set[Addr]:
-        """
-        Provide the set of addresses matched in primary
-        :return: set of addresses in primary
-        """
-        return set(self.primary_idx.keys())
-
-    @property
-    def secondary_address_matched(self) -> Set[Addr]:
-        """
-        Provide the set of addresses matched in the secondary binary
-        :return: set of addresses in secondary
-        """
-        return set(self.secondary_idx.keys())
-
-    @property
-    def nb_match(self) -> int:
-        """ Returns the number of matches """
-        return len(self.primary_idx)
-
-    @property
-    def nb_unmatched_primary(self) -> int:
-        """ Number of unmatched function in the primary program """
-        return len(self.unmatched_primary)
-
-    @property
-    def nb_unmatched_secondary(self) -> int:
-        """ Number of unmatched function in the secondary program """
-        return len(self.unmatched_secondary)
-
-    @property
-    def nb_function_primary(self) -> int:
-        """ Total number of function in primary """
-        return self.nb_match + self.nb_unmatched_primary
-
-    @property
-    def nb_function_secondary(self) -> int:
-        """ Total number of function in secondary """
-        return self.nb_match + self.nb_unmatched_secondary
-
-    def match_primary(self, addr: Addr) -> Optional[Match]:
-        """ Returns the match object associated with the given primary function address """
-        try:
-            return self.primary_idx[addr]
-        except KeyError:
-            return None
-
-    def match_secondary(self, addr: Addr) -> Optional[Match]:
-        """ Returns the match object associated with the given secondary function address """
-        try:
-            return self.secondary_idx[addr]
-        except KeyError:
-            return None
-
-    def is_match_primary(self, addr: Addr) -> bool:
-        """ Returns true if the address in primary did match with a function """
-        return addr in self.primary_idx
-
-    def is_match_secondary(self, addr: Addr) -> bool:
-        """ Returns true if the address in secondary did match with a function in primary """
-        return addr in self.secondary_idx
-
-    def is_unmatch_primary(self, addr: Addr) -> bool:
-        """ Returns true if the address is an unmatched function in primary """
-        return addr in self.unmatched_primary
-
-    def is_unmatch_secondary(self, addr: Addr) -> bool:
-        """ Returns true if the address is an unmatched function in secondary """
-        return addr in self.unmatched_secondary
-
-    def load_file(self, file: str) -> None:
-        """ Load the given JSON file which contains matching data """
-        with open(file, 'r') as f:
-            data = json.load(f)
-        self.global_sim = data['similarity']
-        for entry in data["matches"]:
-            addr1, addr2 = entry['addr1'], entry['addr2']
-            if addr1 is not None:  # add all addresses in unmatched (will be removed by add_match)
-                self.unmatched_secondary.add(entry['addr2'])
-            if addr2 is not None:
-                self.unmatched_primary.add(entry['addr1'])
-            if addr1 and addr2:
-                self.add_match(addr1, addr2, entry['similarity'])
-
-    def write_file(self, out_file: str) -> None:
-        """ Output the matching to a json file """
-        data = {'similarity': float("{0:.2f}".format(self.similarity)), 'matches': []}
-        for match in self.primary_idx.values():
-            data['matches'].append({'addr1': match.addr_primary,
-                                    'addr2': match.addr_secondary,
-                                    'similarity': match.similarity})
-        for un_p1 in self.unmatched_primary:
-            data['matches'].append({'addr1': un_p1, 'addr2': None, 'similarity': 0.0})
-        for un_p2 in self.unmatched_secondary:
-            data['matches'].append({'addr1': None, 'addr2': un_p2, 'similarity': 0.0})
-
-        with open(out_file, "w") as out:
-            json.dump(data, out)
