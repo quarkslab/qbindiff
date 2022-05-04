0496726f17060fb416f17fa605d5dddc8be95f94
Riccardo Mori <rmori@quarkslab.com>
2022-05-04 12:34:16 +0200

    Add FunctionBackendQBinExport.is_library() implementation

--- qbindiff/loader/backend/qbinexport.py
+++ qbindiff/loader/backend/qbinexport.py
@@ -178,11 +178,16 @@ class FunctionBackendQBinExport(AbstractFunctionBackend):
     def type(self, value: FunctionType) -> None:
         self._type = value
 
-    @property
     def is_import(self) -> bool:
         """True if the function is imported"""
         # Should we consider also FunctionType.library?
         if self.type in (FunctionType.imported, FunctionType.thunk):
+            return True
+        return False
+
+    def is_library(self) -> bool:
+        """True if the function is a library function"""
+        if self.type == FunctionType.library:
             return True
         return False
 
