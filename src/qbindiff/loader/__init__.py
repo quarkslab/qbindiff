5b30e91cbcc3ce92a73f2eff03d156c4ed8c0af9
Riccardo Mori <rmori@quarkslab.com>
2022-06-10 11:49:34 +0200

    Import Structure and StructureMember in __init__.py

--- src/qbindiff/loader/__init__.py
+++ src/qbindiff/loader/__init__.py
@@ -1,9 +1,10 @@
+from qbindiff.loader.data import Data
+from qbindiff.loader.structure import Structure, StructureMember
 from qbindiff.loader.operand import Operand
 from qbindiff.loader.instruction import Instruction
 from qbindiff.loader.function import Function
 from qbindiff.loader.basic_block import BasicBlock
 from qbindiff.loader.program import Program
-from qbindiff.loader.data import Data
 from qbindiff.loader.types import LoaderType
 
 LOADERS = {
