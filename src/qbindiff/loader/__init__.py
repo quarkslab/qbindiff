b56c686363cbdc4110f4902310018acfc77831f8
Riccardo Mori <rmori@quarkslab.com>
2022-06-16 09:58:33 +0200

    Reordering of import

--- src/qbindiff/loader/__init__.py
+++ src/qbindiff/loader/__init__.py
@@ -2,8 +2,8 @@ from qbindiff.loader.data import Data
 from qbindiff.loader.structure import Structure, StructureMember
 from qbindiff.loader.operand import Operand
 from qbindiff.loader.instruction import Instruction
-from qbindiff.loader.function import Function
 from qbindiff.loader.basic_block import BasicBlock
+from qbindiff.loader.function import Function
 from qbindiff.loader.program import Program
 from qbindiff.loader.types import LoaderType
 
