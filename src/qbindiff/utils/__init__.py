919e9d706dcead62a09ef0c2dd0de62e1b124d97
Riccardo Mori <rmori@quarkslab.com>
2022-10-03 14:08:14 +0200

    Move iter_csr_matrix on utils

--- src/qbindiff/utils/__init__.py
+++ src/qbindiff/utils/__init__.py
@@ -1,1 +1,1 @@
-from .utils import is_debug
+from .utils import is_debug, iter_csr_matrix
--- src/qbindiff/utils/utils.py
+++ src/qbindiff/utils/utils.py
@@ -4,3 +4,14 @@ import logging
 def is_debug() -> bool:
     """Returns True if the current logging level is set to debug"""
     return logging.root.level <= logging.DEBUG
+
+
+def iter_csr_matrix(matrix):
+    """
+    Iter over non-null items in a CSR (Compressed Sparse Row) matrix.
+    It returns a generator that, at each iteration, returns the tuple
+    (row_index, column_index, value)
+    """
+    coo_matrix = matrix.tocoo()
+    for x, y, v in zip(coo_matrix.row, coo_matrix.col, coo_matrix.data):
+        yield (x, y, v)
