91216ffa2ea413504c3101f03676d9a7819b50c7
Riccardo Mori <rmori@quarkslab.com>
2022-10-03 13:56:20 +0200

    Add LoaderType from root module

--- src/qbindiff/__init__.py
+++ src/qbindiff/__init__.py
@@ -2,4 +2,5 @@ from qbindiff.abstract import GenericGraph
 from qbindiff.differ import QBinDiff, DiGraphDiffer, Differ
 from qbindiff.mapping import Mapping
 from qbindiff.loader import Program, Function
+from qbindiff.loader.types import LoaderType
 from qbindiff.matcher import Matcher
