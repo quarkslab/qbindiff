3b4facc30f9751f9e0165af06b9be922c862c979
rcohen <rcohen@quarkslab.com>
2023-03-08 17:19:20 +0000

    Refactoring topology.py

--- src/qbindiff/features/topology.py
+++ src/qbindiff/features/topology.py
@@ -5,49 +5,81 @@ from qbindiff.loader import Program, Function
 
 
 class ChildNb(FunctionFeatureExtractor):
-    """Number of children of the function"""
 
     key = "cnb"
 
     def visit_function(
         self, program: Program, function: Function, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Number of children of the function
+
+        :param program: program to consider
+        :param function: function of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         value = len(function.children)
         collector.add_feature(self.key, value)
 
 
 class ParentNb(FunctionFeatureExtractor):
-    """Number of parents of the function"""
 
     key = "pnb"
 
     def visit_function(
         self, program: Program, function: Function, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Number of parents of the function
+
+        :param program: program to consider
+        :param function: function of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         value = len(function.parents)
         collector.add_feature(self.key, value)
 
 
 class RelativeNb(FunctionFeatureExtractor):
-    """Number of relatives of the function"""
 
     key = "rnb"
 
     def visit_function(
         self, program: Program, function: Function, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Number of relatives of the function
+
+        :param program: program to consider
+        :param function: function of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         value = len(function.parents) + len(function.children)
         collector.add_feature(self.key, value)
 
 
 class LibName(FunctionFeatureExtractor):
-    """Call to library functions (local function)"""
 
     key = "lib"
 
     def visit_function(
         self, program: Program, function: Function, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Call to library functions (local function)
+
+        :param program: program to consider
+        :param function: function of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         value = defaultdict(int)
         for addr in function.children:
             if program[addr].is_library():
@@ -56,13 +88,21 @@ class LibName(FunctionFeatureExtractor):
 
 
 class ImpName(FunctionFeatureExtractor):
-    """Call to imported functions"""
 
     key = "imp"
 
     def visit_function(
         self, program: Program, function: Function, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Call to imported functions
+
+        :param program: program to consider
+        :param function: function of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         value = defaultdict(int)
         for addr in function.children:
             if program[addr].is_import():
