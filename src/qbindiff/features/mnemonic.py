e5b49024cd11cd5cf451a61eb99c1f05ce819cf5
rcohen <rcohen@quarkslab.com>
2023-03-08 17:18:49 +0000

    Refactoring mnemonic.py

--- src/qbindiff/features/mnemonic.py
+++ src/qbindiff/features/mnemonic.py
@@ -5,41 +5,65 @@ from qbindiff.loader import Program, Instruction
 
 
 class MnemonicSimple(InstructionFeatureExtractor):
-    """Mnemonic of instructions feature"""
 
     key = "M"
 
     def visit_instruction(
         self, program: Program, instruction: Instruction, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Mnemonic of instructions feature
+
+        :param program: program to consider
+        :param instruction: instruction of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         collector.add_dict_feature(self.key, {instruction.mnemonic: 1})
 
 
 class MnemonicTyped(InstructionFeatureExtractor):
-    """Mnemonic and type of operand feature"""
 
     key = "Mt"
 
     def visit_instruction(
         self, program: Program, instruction: Instruction, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Mnemonic and type of operand feature
+
+        :param program: program to consider
+        :param instruction: instruction of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         # Use keys as string so it can later be sorted
         op_types = defaultdict(int)
         for op in instruction.operands:
             op_types[str(op.type)] += 1
         op_types[instruction.mnemonic] = 1
-        key = hash(frozenset(sorted(op_types.items())))
+        key = str(hash(frozenset(sorted(op_types.items()))))  # Key should be a str, not an int returned by hash
         collector.add_dict_feature(self.key, {key: 1})
 
 
 class GroupsCategory(InstructionFeatureExtractor):
-    """Group of the instruction (FPU, SSE, stack..)"""
 
     key = "Gp"
 
     def visit_instruction(
         self, program: Program, instruction: Instruction, collector: FeatureCollector
-    ):
+    ) -> None:
+        """
+        Group of the instruction (FPU, SSE, stack..)
+
+        :param program: program to consider
+        :param instruction: instruction of the program from which we want to extract the feature
+        :param collector: collector to register features
+        :return: None
+        """
+
         for key in instruction.groups:
             if key not in ["UNDEFINED", "NOTINCS", "NOTINIDA", "DEPRECATED"]:
-                collector.add_dict_feature(self.key, {key: 1})
+                collector.add_dict_feature(self.key, {str(key): 1})  # Key should be a str, not an int returned by hash
