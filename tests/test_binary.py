5233e0d0363e25d4800fac524a9b49d3c3a1155b
Riccardo Mori <patacca@autistici.org>
2023-06-26 11:21:10 +0200

    Add Binexport test and fix API

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -61,3 +61,43 @@ class TestQuokka:
             expected = json.load(fp)
 
         assert not (output ^ set(tuple(e) for e in expected))
+
+
+class TestBinExport:
+    """Regression tests for BinExport"""
+
+    base_path = BASE_TEST_PATH / "binaries"
+    # We need the address heuristic with a very low weight to discern between nearly
+    # identical functions
+    features = [
+        (WeisfeilerLehman, 1.0, Distance.cosine),
+        (Constant, 1.0, Distance.canberra),
+        (Address, 0.01, Distance.canberra),
+    ]
+
+    def path(self, p: str) -> Path:
+        return self.base_path / p
+
+    def test_x86_binary(self):
+        primary = Program.from_binexport(self.path("test-bin.BinExport"))
+        secondary = Program.from_binexport(self.path("test-bin2.BinExport"))
+        differ = QBinDiff(
+            primary,
+            secondary,
+            sparsity_ratio=0.75,
+            tradeoff=0.75,
+            epsilon=0.5,
+            distance=Distance.canberra,
+        )
+
+        for f, w, d in self.features:
+            differ.register_feature_extractor(f, w, distance=d)
+
+        differ.process()
+        mapping = differ.compute_matching()
+        output = {(match.primary.addr, match.secondary.addr) for match in mapping}
+
+        with open(self.path("test-bin-binexport.results")) as fp:
+            expected = json.load(fp)
+
+        assert not (output ^ set(tuple(e) for e in expected))
